7c0519f5a1fd8e9120a0679edc9c391991e401da
Chen <gip98507123@gmail.com>
2024-02-21 15:46:14 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/css/index.css';
 import App from './App.vue';
 import router from './router';
 
 // axios所需要將格式改成如下
 const app = createApp(App);
 app.use(VueAxios, axios);
+app.component('LoadingAnimation', Loading);
 app.use(router);
 app.mount('#app');
