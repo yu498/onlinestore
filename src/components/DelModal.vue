8149aacd1960173779b3815ce6b9247d18b5438e
Chen <gip98507123@gmail.com>
2024-02-21 15:44:55 +0800

    新增刪除功能

---
+++ src/components/DelModal.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="modal fade" id="exampleModal" tabindex="-1"
+aria-labelledby="exampleModalLabel" aria-hidden="true" ref="modal">
+  <div class="modal-dialog">
+    <div class="modal-content border-0">
+      <div class="modal-header bg-danger text-white">
+        <h5 class="modal-title" id="exampleModalLabel"><span>刪除{{ item.title }}</span></h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        是否刪除<strong class="text-danger">{{ item.title }}</strong>(刪除後無法復原)
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+          取消
+        </button>
+        <button type="button" class="btn bt-danger" @click="$emit('del-item')">確認刪除</button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import modalMixin from '@/mixins/modalMixin';
+
+export default {
+  props: {
+    item: {},
+  },
+  data() {
+    return {
+      modal: '',
+    };
+  },
+  mixins: [modalMixin],
+};
+</script>
