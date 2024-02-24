cfc0754ae81d6a32f189b18d7655d1c65283225a
Chen <gip98507123@gmail.com>
2024-02-24 20:30:12 +0800

    後台訂單與優惠卷頁面

---
+++ src/views/CouponView.vue
@@ -0,0 +1,32 @@
+<template>
+  <table class="table mt-4">
+<thead>
+  <tr>
+    <th>名稱</th>
+    <th>折扣百分比</th>
+    <th>到期日</th>
+    <th>是否啟用</th>
+    <th>編輯</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td></td>
+    <td class="text-right">
+    </td>
+    <td class="text-right">
+    </td>
+    <td>
+      <span class="text-success">啟用</span>
+      <span class="text-success text-muted">未啟用</span>
+    </td>
+    <td>
+      <div class="btn-group">
+        <button class="btn btn-outline-primary btn-sm">編輯</button>
+        <button class="btn btn-outline-danger btn-sm">刪除</button>
+      </div>
+    </td>
+  </tr>
+</tbody>
+</table>
+</template>
---
+++ src/views/OrderView.vue
@@ -0,0 +1,34 @@
+<template>
+    <table class="table mt-4">
+  <thead>
+    <tr>
+      <th width="120">購買時間</th>
+      <th>Email</th>
+      <th width="120">購買款項</th>
+      <th width="120">應付金額</th>
+      <th width="100">是否付款</th>
+      <th width="200">編輯</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td></td>
+      <td></td>
+      <td class="text-right">
+      </td>
+      <td class="text-right">
+      </td>
+      <td>
+        <span class="text-success">啟用</span>
+        <span class="text-success text-muted">未啟用</span>
+      </td>
+      <td>
+        <div class="btn-group">
+          <button class="btn btn-outline-primary btn-sm">編輯</button>
+          <button class="btn btn-outline-danger btn-sm">刪除</button>
+        </div>
+      </td>
+    </tr>
+  </tbody>
+</table>
+</template>
